6991f4e71d85027f1eaeffba13246589436a581a
PEDRO HENRIQUE PIFFER HARADA <pifferpedro2@gmail.com>
2026-04-12 19:22:30 -0300

    função + laço

---
+++ ex3.py
@@ -0,0 +1,6 @@
+def soma_de_numero(n):
+    soma= 0
+    for i in range(1,n+1):
+        soma+=i
+    return soma
+print(soma_de_numero(67))
---
+++ ex4.py
@@ -0,0 +1,6 @@
+def contador():
+    num = 0
+    while num <=20:
+        print(num)
+        num += 1
+contador()
